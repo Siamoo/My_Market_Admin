0972bc8db4b8547893ebe383bfc90f8a633b0e3b
Siamoo <walidsyam.f@gmail.com>
2025-09-11 04:01:21 +0300

    Create login cubit & login fun

---
+++ lib/features/auth/cubit/cubit/login_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_market_admin/core/functions/api_services.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+final ApiServices _apiServices = ApiServices();
+  Future<void> login(Map<String, dynamic> data) async {
+    emit(LoginLoading());
+    try {
+      Response response = await _apiServices.login('token', data );
+      if (response.statusCode == 200) {
+        emit(LoginSuccess());
+      } else {
+        emit(LoginError(response.data['msg']));
+      }
+    } catch (e) {
+      emit(LoginError(e.toString()));
+    }
+  }
+}
