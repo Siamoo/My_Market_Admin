65b3e0be810cf948cd3a5deb070bc50a3b307e2e
Siamoo <walidsyam.f@gmail.com>
2025-09-14 01:50:37 +0300

    Add MyObserver to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_market_admin/core/functions/my_observer.dart';
 import 'package:my_market_admin/features/auth/views/login_view.dart';
 
 void main() {
@@ -11,11 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Bloc.observer = MyObserver();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My Market Admin',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: LoginView(),
