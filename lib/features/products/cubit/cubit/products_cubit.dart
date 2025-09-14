1107e57f23655df944c6d34390b8a92dbd8f4ab1
Siamoo <walidsyam.f@gmail.com>
2025-09-14 08:44:22 +0300

    Upload image to supabase using dio package

--- lib/features/products/cubit/cubit/products_cubit.dart
+++ lib/features/products/cubit/cubit/products_cubit.dart
@@ -1,6 +1,10 @@
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_market_admin/core/functions/api_services.dart';
+import 'package:my_market_admin/core/functions/sensitive_data.dart';
 import 'package:my_market_admin/core/functions/shared_pref.dart';
 import 'package:my_market_admin/features/products/models/products.dart';
 
@@ -23,6 +27,46 @@ class ProductsCubit extends Cubit<ProductsState> {
       }
     } catch (e) {
       emit(GetProductsError());
+    }
+  }
+
+  String imageUrl = '';
+  Future<void> uploadimage({
+    required Uint8List image,
+    required String iamgeName,
+    required bucketName,
+  }) async {
+    const String _storageBaseUrl =
+        'https://rmmvawnbnfdsyjbgturm.supabase.co/storage/v1/object';
+    const String apiKey = anonkey;
+    final String? token = await SharedPref.getToken();
+    final String url = '$_storageBaseUrl/$bucketName/$iamgeName';
+    final Dio _dio = Dio();
+    final FormData formData = FormData.fromMap({
+      'file': MultipartFile.fromBytes(image, filename: iamgeName),
+    });
+    emit(UploadImageLoading());
+
+    try {
+      Response response = await _dio.post(
+        url,
+        data: formData,
+        options: Options(
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'apikey': apiKey,
+            'Authorization': 'Bearer $token',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        imageUrl = 'https://rmmvawnbnfdsyjbgturm.supabase.co/storage/v1/object/public/${response.data['Key']}';
+        
+        print(response.data['Key']);
+        emit(UploadImageSuccess());
+      }
+    } catch (e) {
+      emit(UploadImageError());
     }
   }
 }
