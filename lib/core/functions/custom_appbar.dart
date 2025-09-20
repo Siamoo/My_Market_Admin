196fde6539028aada1c11b1f380417a5e684c3fd
Siamoo <walidsyam.f@gmail.com>
2025-09-20 08:29:14 +0300

    update custom appbar

--- lib/core/functions/custom_appbar.dart
+++ lib/core/functions/custom_appbar.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:my_market_admin/core/functions/app_colors.dart';
+import 'package:my_market_admin/core/functions/navigation_service.dart';
+import 'package:my_market_admin/features/home/views/home_view.dart';
 
 AppBar customAppBar(
   BuildContext context,
   String title, {
-  bool? isBackButton = false,
+  bool? isBackButton = true,
+  bool? isHomeButton = true,
 }) {
   return AppBar(
     centerTitle: true,
-    title: Text(title, style: TextStyle(color: AppColors.kWhiteColor)),
+    title: Text(
+      title,
+      style: const TextStyle(color: AppColors.kWhiteColor),
+    ),
     backgroundColor: AppColors.kPrimaryColor,
     leading: isBackButton == true
         ? IconButton(
@@ -19,5 +25,17 @@ AppBar customAppBar(
             ),
           )
         : Container(),
+    actions: [
+      if (isHomeButton == true)
+        IconButton(
+          onPressed: () {
+            NavigationService.pushReplacementTo(context, HomeView());
+          },
+          icon: const Icon(
+            Icons.home,
+            color: AppColors.kWhiteColor,
+          ),
+        ),
+    ],
   );
 }
